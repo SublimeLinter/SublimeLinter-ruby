e2a5622bc8bba56a816a07cffb4d5f9cf8b19e14
Aparajita Fishman <aparajita@aparajita.com>
2013-12-15 13:14:32 -0800

    Initial commit.

---
+++ linter.py
@@ -0,0 +1,28 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Aparajita
+# Copyright (c) 2013 Aparajita
+#
+# License: MIT
+#
+
+"""This module exports the Ruby plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class Ruby(Linter):
+
+    """Provides an interface to ruby -wc."""
+
+    syntax = 'ruby'
+    cmd = 'ruby -wc'
+    regex = (
+        r'^.+?:(?P<line>\d+):.+?, (?P<message>[^\r\n]+)\r?\n'
+        r'(?:^[^\r\n]+\r?\n^(?P<col>.*?)\^)?'
+    )
+    multiline = True
+    error_stream = util.STREAM_STDERR
+    comment_re = r'\s*#'
