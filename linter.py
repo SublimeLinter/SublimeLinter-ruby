0314d750649b36044688c3064812161ab92205ba
Aparajita Fishman <aparajita@aparajita.com>
2014-01-02 10:58:59 -0500

    Added rspec syntax.

--- linter.py
+++ linter.py
@@ -17,7 +17,7 @@ class Ruby(RubyLinter):
 
     """Provides an interface to ruby -wc."""
 
-    syntax = ('ruby', 'ruby on rails')
+    syntax = ('ruby', 'ruby on rails', 'rspec')
     cmd = 'ruby -wc'
     regex = (
         r'^.+?:(?P<line>\d+): (?:(?P<error>.*?error)|(?P<warning>warning))[,:] (?P<message>[^\r\n]+)\r?\n'
